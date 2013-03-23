4d13a9ee6b3b85392ae01fb5c96b79e372dbcae1
Gerald McAlister <gerald.mcalister.10@cnu.edu>
2013-03-23 14:27:31 -0400

    Delete unused file
    Delete an used test file that was accidentally put in and update the
    build.

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
-using CompressString;
 
 namespace Snake
 {
@@ -14,17 +13,6 @@ namespace Snake
         [STAThread]
         static void Main()
         {
-            string testing = "";
-            for (int i = 0; i < 36; i += 1)
-            {
-                testing += 'a';
-            }
-            int length = testing.Length;
-            testing = StringCompressor.CompressString(testing);
-            length = testing.Length;
-            testing = StringCompressor.DecompressString(testing);
-            length = testing.Length;
-            
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new gameWindowForm());
